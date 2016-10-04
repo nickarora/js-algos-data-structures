5aafebac3b2b1a505a62e1f4305f457899081a2a
Nick Arora <arora.nick@gmail.com>
2016-10-03 18:45:19 -0700

    add set operation methods

--- set/index.js
+++ set/index.js
@@ -31,6 +31,36 @@ class Set {
   values() {
     return Object.values(this.items)
   }
+
+  union(otherSet) {
+    const unionSet = new Set()
+    this.values().forEach(value => unionSet.add(value))
+    otherSet.values().forEach(value => unionSet.add(value))
+    return unionSet
+  }
+
+  intersection(otherSet) {
+    const intersectionSet = new Set()
+    this.values()
+      .filter(value => otherSet.has(value))
+      .forEach(value => intersectionSet.add(value))
+    return intersectionSet
+  }
+
+  // exists in this set but not in otherSet
+  difference(otherSet) {
+    const intersectionSet = new Set()
+    this.values()
+      .filter(value => !otherSet.has(value))
+      .forEach(value => intersectionSet.add(value))
+    return intersectionSet
+  }
+
+  subset(otherSet) {
+    if (this.size() > otherSet.size()) return false
+    return this.values()
+      .every(value => otherSet.has(value))
+  }
 }
 
 export default Set
